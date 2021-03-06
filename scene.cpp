1cddf4eb264ba644b0b8c3cfde09173ce3efcd93
Joe Guida <JoeGuida@outlook.com>
2021-03-06 16:56:27 -0500

    Modularize phong/diffuse, add specular

--- scene.cpp
+++ scene.cpp
@@ -4,22 +4,8 @@
 #include <iostream>
 #include <cmath>
 
-float Diffuse(const Vec3& lightDir, const Vec3& normal) {
-	return std::max(0.0f, Dot(normal, -lightDir));
-}
-
-float Phong(const Vec3& lightDir, const Vec3& normal, Vec3 e) {
-	int p = 256;
-	Vec3 r = Reflection(lightDir, normal);
-	float angle = std::max(0.0f, Dot(e, r));
-	return powf(angle, p);
-}
-
-bool isInShadow(const Ray& ray, std::vector<Sphere> spheres, const Sphere& surface) {
+bool castRay(const Ray& ray, const Hit& hit, const std::vector<Sphere>& spheres) {
 	for (const Sphere& sphere : spheres) {
-		if (surface.position == sphere.position)
-			continue;
-
 		Hit hit;
 		if (sphere.Intersect(ray, hit)) {
 			return true;
@@ -28,36 +14,69 @@ bool isInShadow(const Ray& ray, std::vector<Sphere> spheres, const Sphere& surfa
 	return false;
 }
 
+Color Diffuse(const Vec3& lightDir, const Vec3& normal, const Color& surfaceColor) {
+	float diffuseValue = std::max(0.0f, Dot(normal, -lightDir));
+	return surfaceColor * diffuseValue;
+}
+
+Color Phong(const Vec3& lightDir, const Vec3& normal, Vec3 e, const Color& lightColor) {
+	int p = 256;
+	Vec3 r = Reflection(lightDir, normal);
+	float angle = std::max(0.0f, Dot(e, r));
+	float phongValue = powf(angle, p);
+	return lightColor * phongValue;
+}
+
+Color Specular(const Vec3& d, const Hit& hit, const std::vector<Sphere>& spheres) {
+	Vec3 r = -Reflection(d, hit.normal);
+	Hit rayHit;
+
+	float specularValue = std::max(0.0f, Dot(r, hit.normal));
+	Color specularColor = castRay(Ray(hit.point, Normalized(r)), rayHit, spheres);
+	return specularColor * specularValue;
+}
+
+bool pointInShadow(const Point& hitPoint, const Light& light, const Sphere& surface, const std::vector<Sphere>& spheres) {
+	float shadowBias = 0.1f;
+	Point origin = hitPoint + shadowBias;
+	Ray shadowRay(origin, origin - light.position);
+	bool inShadow = false;
+
+	for (const Sphere& sphere : spheres) {
+		if (surface.position == sphere.position)
+			continue;
+
+		Hit hit;
+		if (sphere.Intersect(shadowRay, hit)) {
+			inShadow = true;
+		}
+	}
+
+	return inShadow;
+}
+
 Color Scene::Trace(int u, int v) {
 	Ray cameraRay(Point(0.0f, 0.0f, 0.0f), Vec3(u, v, -1000));
 
 	for (const Sphere& sphere : spheres) {
 		Hit hit;
 		if (sphere.Intersect(cameraRay, hit)) {
-			float diffuse = 0; 
-			float phong = 0;
-			Color lightColor = Color(0.0f, 0.0f, 0.0f);
+			Color diffuse(0.0f, 0.0f, 0.0f);
+			Color phong(0.0f, 0.0f, 0.0f);
+			Color specular(0.0f, 0.0f, 0.0f);
 
 			for (const Light& light : lights) {
-				// Shadows
-				float shadowBias = 0.1f;
-				Point origin = hit.point + shadowBias;
-				Ray shadowRay(origin, origin - light.position);
-				if (isInShadow(shadowRay, spheres, sphere)) {
-					return Min(ambient + Color(0, 0, 0), 1.0f);
-				}
-
+				if (pointInShadow(hit.point, light, sphere, spheres))
+					return Color(0, 0, 0);
+				
 				Vec3 l = Normalized(hit.point - light.position);
-				diffuse = Diffuse(-l, hit.normal);
-				phong += Phong(l, hit.normal, cameraRay.direction);
-				lightColor += light.color;
+				diffuse = Diffuse(-l, hit.normal, sphere.color);
+				phong = Phong(l, hit.normal, cameraRay.direction, light.color);
+				specular = Specular(cameraRay.direction, hit, spheres);
 			}
-
 			// Components of color
-			Color diffuseColor = sphere.color * diffuse;
-			Color phongColor = lightColor * phong;
-			Color color = ambient + diffuseColor + phongColor;
-
+			Color color = ambient + diffuse + phong + specular;
+			
 			// Clamp color values before returning
 			color = Min(color, 1.0f);
 			return color;
