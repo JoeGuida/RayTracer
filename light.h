c7e289304cebcb7092eacf8b6e58c23d15ba722a
Joe Guida <JoeGuida@outlook.com>
2021-03-05 19:29:13 -0500

    Add light color

--- light.h
+++ light.h
@@ -2,14 +2,16 @@
 #define _LIGHT_H_
 
 #include "point.h"
+#include "color.h"
 
 typedef struct Light {
 	Point position;
 	float intensity;
+	Color color;
 
-	Light() : position(Vec3(0.0f, 0.0f, 0.0f)), intensity(1.0f) {}
-	Light(Point p) : position(p), intensity(1.0f) {}
-	Light(Point p, float i) : position(p), intensity(i) {}
+	Light() : position(Vec3(0.0f, 0.0f, 0.0f)), intensity(1.0f), color(Color(1.0f, 1.0f, 1.0f)) {}
+	Light(Point p) : position(p), intensity(1.0f), color(Color(1.0f, 1.0f, 1.0f)) {}
+	Light(Point p, float i, Color c) : position(p), intensity(i), color(c) {}
 
 } Light;
 
